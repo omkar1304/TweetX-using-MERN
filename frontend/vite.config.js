56bc34786209ad0aa045159c6d8efc067e1d2fe6
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2024-02-03 10:49:12 +0530

    Add config to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": "https://tweetxbackend.vercel.app",
-    },
-  },
-});
+})
 
