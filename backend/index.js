79e4b0a21a119a5c7c51265bcffd2e1174c2c1a2
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2024-02-03 15:33:31 +0530

    Update index.js file

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://tweetxfrontend.netlify.app"],
+  origin: ["https://tweetx-using-mern.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true, 
 }));
