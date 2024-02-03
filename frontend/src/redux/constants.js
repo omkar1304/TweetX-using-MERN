0732132b27d29cacdf9b3e9224243d2edbb3aba9
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2024-02-03 10:30:34 +0530

    Add few changes

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://tweetxfrontend.vercel.app"
+export const BASE_URL = ""
 export const USER_URL = "/api/users"
 export const POST_URL = "/api/posts"
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/api/": "https://tweetxbackend.vercel.app",
+    },
   },
 });
+
