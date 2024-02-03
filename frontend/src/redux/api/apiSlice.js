a62ccb0a664f095e31421b80d5afeb74eb69072d
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2024-02-03 11:04:27 +0530

    Add RTK configuration

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -6,6 +6,7 @@ export const apiSlice = createApi({
   tagTypes: ["followingPosts", "users", "profileFollowers", "profileFollowing", "getUserById"],
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    credentials: 'include',
   }),
   endpoints: () => ({}),
 });
